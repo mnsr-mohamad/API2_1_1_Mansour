f4402a4c794892614aa6b096d1800a820c2c2baa
mansour.m <m.mansourjamaleddine@condorcet.be>
2023-04-15 16:20:05 +0200

    Fin des mvp pour toutes les classes

--- src/mvp/GestCli.java
+++ src/mvp/GestCli.java
@@ -7,23 +7,24 @@ import mvp.presenter.LocalPresenter;
 import mvp.presenter.SessionCoursPresenter;
 import mvp.view.*;
 
+import java.util.Scanner;
+
 public class GestCli {
 
     public static void main(String[] args) {
-       DAOCours cm = new CoursModelDB();
+        DAOCours cm = new CoursModelDB();
         CoursViewInterface cv = new CoursViewConsole();
         CoursPresenter cp = new CoursPresenter(cm,cv);//création et injection de dépendance
-        //cp.start();
+
 
         DAOFormateur fm = new FormateurModelDB();
         FormateurViewInterface fv = new FormateurViewConsole();
         FormateurPresenter fp = new FormateurPresenter(fm,fv);//création et injection de dépendance
-        //fp.start();
+
 
         DAOLocal lo = new LocalModelDB();
         LocalViewInterface lv = new LocalViewConsole();
         LocalPresenter lp= new LocalPresenter(lo,lv);
-
 
 
         DAOSessionCours ss = new SessionCoursModelDB();
@@ -31,8 +32,35 @@ public class GestCli {
         SessionCoursPresenter sp= new SessionCoursPresenter(ss,sv);
         sp.setCoursPresenter(cp);
         sp.setLocalPresenter(lp);
-        sp.start();
 
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("1.Cours\n2.Formateur\n3.Local\n4.SessionCours\n5.Fin");
+
+            int ch = sc.nextInt();
+            sc.skip("\n");
+            switch (ch) {
+                case 1:
+                    cp.start();
+                    break;
+                case 2:
+                    fp.start();
+                    break;
+                case 3:
+
+                    lp.start();
+                    break;
+                case 4:
+                    sp.setCoursPresenter(cp);
+                    sp.setLocalPresenter(lp);
+                    sp.start();
+                    break;
+                case 5:
+                    System.exit(0);
+            }
+        } while (true);
     }
+
+
 }
 
